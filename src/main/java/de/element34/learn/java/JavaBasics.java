57d1bb5c42256214393373c0ae8b5732f5827d79
Stefan Langer <stefan.langer@payback.net>
2021-11-27 19:26:34 +0100

    Lecture 1: Java Basics - printing a message to standard out

---
+++ src/main/java/de/element34/learn/java/JavaBasics.java
@@ -0,0 +1,17 @@
+package de.element34.learn.java;
+
+/**
+ * Course for learning Java
+ * Lecture 1: Java Basics
+ * 1 Printing a message to standard out
+ */
+public class JavaBasics {
+    /**
+     * The main method is the entry point to your java program.
+     * It always has a String array as a parameter and returns nothing.
+     */
+    public static void main(String[] args) {
+        // Print a message to the commandline
+        System.out.println("Hello I am your first Java program!");
+    }
+}
